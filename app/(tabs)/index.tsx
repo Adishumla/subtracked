20aa5a87b1ac2322a5e007284aeb0edd7eee0e0f
Adishumla <45212964+Adishumla@users.noreply.github.com>
2023-09-07 10:50:32 +0200

    added expo auth

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,8 +1,10 @@
 import { StyleSheet } from "react-native";
 import tw from "tailwind-react-native-classnames";
+import * as LocalAuthentication from "expo-local-authentication";
 
 import EditScreenInfo from "../../components/EditScreenInfo";
 import { Text, View } from "../../components/Themed";
+import { SafeAreaView, Alert, Button } from "react-native";
 
 export default function TabOneScreen() {
   return (
@@ -16,6 +18,10 @@ export default function TabOneScreen() {
         darkColor="rgba(255,255,255,0.1)"
       />
       <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <Button
+        title="Authenticate with Face ID"
+        onPress={handleAuthentication}
+      />
     </View>
   );
 }
@@ -36,3 +42,34 @@ const styles = StyleSheet.create({
     width: "80%",
   },
 });
+
+const handleAuthentication = async () => {
+  try {
+    const compatible = await LocalAuthentication.hasHardwareAsync();
+
+    if (compatible) {
+      const enrolled = await LocalAuthentication.isEnrolledAsync();
+
+      if (enrolled) {
+        const result = await LocalAuthentication.authenticateAsync({
+          promptMessage: "Authenticate to continue", // Message displayed to the user
+        });
+
+        if (result.success) {
+          // Authentication was successful
+          Alert.alert("Authentication successful!");
+          // You can navigate to the main screen or perform any other actions here
+        } else {
+          // Authentication failed
+          Alert.alert("Authentication failed.");
+        }
+      } else {
+        Alert.alert("You have not enrolled in biometrics.");
+      }
+    } else {
+      Alert.alert("Biometrics not available on this device.");
+    }
+  } catch (error) {
+    console.error("Authentication error:", error);
+  }
+};
