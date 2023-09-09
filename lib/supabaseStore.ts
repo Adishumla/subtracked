bc0e9cf0ef83f7e8b55f2babf7c42775415f6b8a
Adishumla <45212964+Adishumla@users.noreply.github.com>
2023-09-09 15:42:08 +0200

    fixed env

--- lib/supabaseStore.ts
+++ lib/supabaseStore.ts
@@ -1,10 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
-const supabaseUrl = "https://xjzqjzqjxqjzjxqjzqjz.supabase.co";
-const supabaseAnonKey = "https://eyzowvekoomzjzjxkyhn.supabase.co/";
-("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV5em93dmVrb29temp6anhreWhuIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTM5ODg5NzMsImV4cCI6MjAwOTU2NDk3M30.WcxXmr1zcw-ALFBd0Ma9u2tpZrFje64BN5WUTZssq10");
-const supabase = createClient(supabaseUrl, supabaseAnonKey);
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL as string;
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY as string;
 
-/* process.env.SUPABASE_URL as string,
-  process.env.SUPABASE_ANON_KEY as string */
+const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 export default supabase;
