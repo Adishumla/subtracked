6c049ec052ec9ef8d7006cfa2ce161fbc43545ab
Adishumla <45212964+Adishumla@users.noreply.github.com>
2023-09-12 22:02:33 +0200

    cleaned pachage and fixed web error

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,41 +1,43 @@
-import 'react-native-url-polyfill/auto'
-import tw from 'tailwind-react-native-classnames'
-import { useState, useEffect } from 'react'
-import { supabase } from '../../lib/supabase'
-import Auth from '../../components/Auth/EmailAuth'
-import { View, Text, Button } from 'react-native'
-import { Session } from '@supabase/supabase-js'
+import "react-native-url-polyfill/auto";
+import tw from "tailwind-react-native-classnames";
+import { useState, useEffect } from "react";
+import supabase from "../../lib/supabaseStore";
+import Auth from "../../components/Auth/EmailAuth";
+import { View, Text, Button } from "react-native";
+import { Session } from "@supabase/supabase-js";
 
 export default function App() {
-  const [session, setSession] = useState<Session | null>(null)
-  const [email, setEmail] = useState<String | undefined>('')
+  const [session, setSession] = useState<Session | null>(null);
+  const [email, setEmail] = useState<String | undefined>("");
 
-  
   useEffect(() => {
     supabase.auth.getSession().then(({ data: { session } }) => {
-      setSession(session)
-      setEmail(session?.user.email)
-      console.log(session)
-// @ts-ignore
-const { data, error } =  supabase
-.from('login')
-.insert([
-  { email: email, Users: session?.user.id},
-])
-.select()
-console.log(error)
-
-    })
+      setSession(session);
+      setEmail(session?.user.email);
+      console.log(session);
+      // @ts-ignore
+      const { data, error } = supabase
+        .from("login")
+        .insert([{ email: email, Users: session?.user.id }])
+        .select();
+      console.log(error);
+    });
 
     supabase.auth.onAuthStateChange((_event, session) => {
-      setSession(session)
-    })
-  }, [])
+      setSession(session);
+    });
+  }, []);
 
   return (
     <View>
       <Auth />
-      <Button title="Sign Out" onPress={() => {supabase.auth.signOut(); console.log(session)}} />
+      <Button
+        title="Sign Out"
+        onPress={() => {
+          supabase.auth.signOut();
+          console.log(session);
+        }}
+      />
     </View>
-  )
+  );
 }
--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,27 +1,29 @@
-import 'react-native-url-polyfill/auto'
-import * as SecureStore from 'expo-secure-store'
-import { createClient } from '@supabase/supabase-js'
+/* import "react-native-url-polyfill/auto";
+import * as SecureStore from "expo-secure-store";
+import { createClient } from "@supabase/supabase-js";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const ExpoSecureStoreAdapter = {
   getItem: (key: string) => {
-    return SecureStore.getItemAsync(key)
+    return SecureStore.getItemAsync(key);
   },
   setItem: (key: string, value: string) => {
-    SecureStore.setItemAsync(key, value)
+    SecureStore.setItemAsync(key, value);
   },
   removeItem: (key: string) => {
-    SecureStore.deleteItemAsync(key)
+    SecureStore.deleteItemAsync(key);
   },
-}
+};
 
 const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL as string;
 const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY as string;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
-    storage: ExpoSecureStoreAdapter as any,
+    storage: AsyncStorage as any,
     autoRefreshToken: true,
     persistSession: true,
     detectSessionInUrl: false,
   },
-})
+});
+ */
