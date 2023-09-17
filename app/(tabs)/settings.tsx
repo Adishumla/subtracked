1d4a7b69c4d3873fc0d01ffe218d7e1b42a977a5
Adishumla <45212964+Adishumla@users.noreply.github.com>
2023-09-17 19:19:30 +0200

    added switches

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,20 +1,23 @@
 // TO DO : Line 24 && 31 - check line comment.
 
 import { View, StyleSheet, Text, ScrollView } from "react-native";
-import {Link} from 'expo-router';
+import { Link } from "expo-router";
 import supabase from "../../lib/supabaseStore";
 import tw from "twrnc";
 import H1 from "../../components/H1";
 import H2 from "../../components/H2";
 import H4 from "../../components/H4";
-import { Button, Input } from "react-native-elements";
+import { Button, Input, Switch } from "react-native-elements";
 import { useState, useEffect } from "react";
 
 export default function App() {
-
+  const [notifications, setNotifications] = useState<boolean>(false);
+  const [darkMode, setDarkMode] = useState<boolean>(false);
   return (
     <ScrollView style={tw`px-4 pt-8`}>
-      <Link href="/(tabs)/overview"><H4 content="< Tillbaka"></H4></Link>
+      <Link href="/(tabs)/overview">
+        <H4 content="< Tillbaka"></H4>
+      </Link>
       <H1 content={"Inställningar"}></H1>
 
       <View style={tw`mt-16 gap-8`}>
@@ -22,20 +25,31 @@ export default function App() {
           <H2 content="Dark mode"></H2>
 
           {/* Handle framemotion toggle switch, save to database as bool */}
-          <H2 content="AV/PÅ"></H2>
+          <Switch
+            //color="#00FF00"
+            value={darkMode}
+            onValueChange={(value) => setDarkMode(value)}
+          />
         </View>
 
         <View style={tw`flex-row justify-between`}>
           <H2 content="Pushnotiser"></H2>
 
           {/* Handle framemotion toggle switch, save to database as bool */}
-          <H2 content="AV/PÅ"></H2>
+          <Switch
+            //color="#00FF00"
+            value={notifications}
+            onValueChange={(value) => setNotifications(value)}
+          />
         </View>
       </View>
 
       <View style={tw`mt-16`}>
         <H2 content="Mejladress"></H2>
-        <Input style={tw`rounded-xl`} placeholder="Ex: throwawayjens@kekleo.tv"></Input>
+        <Input
+          style={tw`rounded-xl`}
+          placeholder="Ex: throwawayjens@kekleo.tv"
+        ></Input>
       </View>
 
       <View>
@@ -44,15 +58,15 @@ export default function App() {
       </View>
 
       <View style={tw`bg-[#DAD8D8] flex mt-16 p-4`}>
-        <Text style={tw``}>För mer information gällande GDPR och hur vi använder dina uppgifter. </Text>
-        <Link style={tw`self-end underline`} href="http://www.google.se">Läs mer här</Link>
+        <Text style={tw``}>
+          För mer information gällande GDPR och hur vi använder dina uppgifter.{" "}
+        </Text>
+        <Link style={tw`self-end underline`} href="http://www.google.se">
+          Läs mer här
+        </Link>
       </View>
 
-      <Button
-        style={tw`my-12`}
-        title="Spara"
-      />
-
+      <Button style={tw`my-12`} title="Spara" />
     </ScrollView>
   );
 }
